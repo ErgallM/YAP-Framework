2746bb00616edd7770b0afaa9494c03b6784d289
ErgallM <ncwlife@gmail.com>
2011-04-26 17:07:13 +0400

    add Yap\Config\Ini

--- library/Yap/Config/Ini.php
+++ library/Yap/Config/Ini.php
@@ -14,34 +14,50 @@ class Ini extends \Yap\Config
         $content = parse_ini_file($fileName, true);
         $config = array();
 
-        function parserIniFileName($name, $value)
+        function parserNodeName($name, $value)
         {
-            if (false === ($pos = strpos($name, ':'))) {
+            echo $name . PHP_EOL;
+
+            if (false === ($pos = strpos($name, '.'))) {
+                if ('[]' == $value) {
+                    $value = array();
+                } else if ('[' == substr($value, 0, 1) && ']' == substr($value, -1)) {
+                    $value = explode(',', substr($value, 1, -1));
+                }
+                
                 return array($name => $value);
-
             } else {
-
-                $nodeName = substr($name, 0, $pos - 1);
-                $name = substr($name, $pos);
-                return array($nodeName => parserIniFileName($name, $value));
+                $nodeName = substr($name, 0, $pos);
+                $name = substr($name, $pos + 1);
+                return array($nodeName => parserNodeName($name, $value));
             }
         };
 
-        function parserIniFile($options) {
-            $result = array();
+        foreach ($content as $nodeName => $nodeValue) {
+            $nodeData = array();
 
-            foreach ($options as $key => $value) {
-                if (is_string($key) && is_string($value)) {
-                    $result[$key] = $value;
+            if (is_string($nodeName) && is_string($nodeValue)) {
+                $nodeData = parserNodeName($nodeName, $nodeValue);
 
-                } else if (is_string($key) && is_array($value)) {
-                    
+            } elseif (is_string($nodeName) && is_array($nodeValue)) {
+
+                if (false !== ($pos = strpos($nodeName, ':'))) {
+                    $parentNodeName = substr($nodeName, $pos + 1);
+                    $nodeName = substr($nodeName, 0, $pos);
+
+                    if (!isset($config[$parentNodeName])) throw new \Exception("Can't found section '$parentNodeName'");
+                    $nodeData[$nodeName] = $config[$parentNodeName];
                 }
 
-            }
-        };
+                if (!isset($nodeData[$nodeName])) $nodeData[$nodeName] = array();
 
-        $config = parserIniFile($content);
+                foreach ($nodeValue as $key => $value) {
+                    $nodeData[$nodeName] = array_merge_recursive($nodeData[$nodeName], parserNodeName($key, $value));
+                }
+            }
+
+            $config = array_merge($config, $nodeData);
+        }
 
         return $config;
     }
