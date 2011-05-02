aecf5778dfb7b9e699f560b725c7e18d6044bf7d
Victor <stalin1945@gmail.com>
2011-05-03 00:06:59 +0300

    Refactoring: Route class refactored with getters/setters

--- library/Yap/Router/Route.php
+++ library/Yap/Router/Route.php
@@ -14,20 +14,14 @@ class Route implements RouteInterface
 
     public function __construct($name, $route, $defaults = null, $reqs = null)
     {
-        $this->_name = $name;
-        $this->_route = $route = trim($route, '/');
-        if (is_array($defaults)) $this->_defaults = $defaults;
-        if (is_array($reqs)) $this->_reqs = $reqs;
-
-        if (false !== ($pos = strpos($route, ':'))) {
-            $this->_static = trim(substr($route, 0, $pos), '/');
-            $route = substr($route, strlen($this->_static));
-        } else {
-            if (false === strpos($route, '*')) {
-                $this->_static = trim($route, '/');
-                $this->_isStatic = true;
-            }
+        $this->setName($name);
+        if (is_array($defaults)) {
+            $this->setDefaults($defaults);
         }
+        if (is_array($reqs)) {
+            $this->_reqs = $reqs;
+        }
+        $this->setRoute($route);
     }
 
     /**
@@ -43,14 +37,18 @@ class Route implements RouteInterface
 
         // Если роутер полностью статичен
         if ($this->_isStatic) {
-            if ($this->_static != $path) return false;
+            if ($this->_static != $path) {
+                return false;
+            }
             
             return $this->_defaults;
         }
 
         // Проверка статичной части
         if (null !== $this->_static) {
-            if (0 !== strpos($path, $this->_static)) return false;
+            if (0 !== strpos($path, $this->_static)) {
+                return false;
+            }
             $route = trim(substr($route, strlen($this->_static)), '/');
             $path = trim(substr($path, strlen($this->_static)), '/');
         }
@@ -77,13 +75,21 @@ class Route implements RouteInterface
                 $varName = substr($var, 1);
 
                 // Если для переменной нету значиня по умолчанию и кусок пути пустой
-                if (!isset($this->_defaults[$varName]) && empty($part)) return false;
+                if (!isset($this->_defaults[$varName]) && empty($part)) {
+                    return false;
+                }
 
-                if (isset($this->_reqs[$varName])) $var = $this->_reqs[$varName];
-                else $var = '.*';
+                if (isset($this->_reqs[$varName])) {
+                    $var = $this->_reqs[$varName];
+                }
+                else {
+                    $var = '.*';
+                }
 
                 // Если для переменной есть значение по умолчанию и кусок пути пустой
-                if (isset($this->_defaults[$varName]) && empty($part)) $part = $this->_defaults[$varName];
+                if (isset($this->_defaults[$varName]) && empty($part)) {
+                    $part = $this->_defaults[$varName];
+                }
 
                 $params[$varName] = $part;
             } elseif ('*' == substr($var, 0, 1)) {
@@ -94,7 +100,9 @@ class Route implements RouteInterface
                     continue;
                 }
             } else {
-                if ($var != $part) return false;
+                if ($var != $part) {
+                    return false;
+                }
             }
 
             $match .= (empty($match)) ? "($var)" : "\/($var)";
@@ -102,7 +110,9 @@ class Route implements RouteInterface
         }
 
         // Если есть дополнительные переменные, а их вводить нельзя
-        if (sizeof($pathArray) && !$last) return false;
+        if (sizeof($pathArray) && !$last) {
+            return false;
+        }
 
 
         echo '{' . $match . '}';
@@ -118,7 +128,9 @@ class Route implements RouteInterface
                     $value = array_shift($pathArray);
 
                     // Дополнительными переменными нельзя сбить основные
-                    if (!isset($params[$key])) $params[$key] = $value;
+                    if (!isset($params[$key])) {
+                        $params[$key] = $value;
+                    }
                 }
             }
 
@@ -129,9 +141,58 @@ class Route implements RouteInterface
             return false;
         }
     }
+    
+    public function setName($name)
+    {
+        $this->_name = $name;
+        return $this;
+    }
 
     public function getName()
     {
         return $this->_name;
+    }
+
+    public function setDefaults($defaults)
+    {
+        $this->_defaults = $defaults;
+        return $this;
+    }
+
+    public function getDefaults()
+    {
+        return $this->_defaults;
+    }
+
+    public function isStatic()
+    {
+        return $this->_isStatic;
+    }
+
+    public function setRoute($route)
+    {
+        $this->_route = $route = trim($route, '/');
+
+        if (false !== ($pos = strpos($route, ':'))) {
+            $this->_static = trim(substr($route, 0, $pos), '/');
+            $route = substr($route, strlen($this->_static));
+        } else {
+            if (false === strpos($route, '*')) {
+                $this->_static = trim($route, '/');
+                $this->_isStatic = true;
+            }
+        }
+        
+        return $this;
+    }
+
+    public function getRoute()
+    {
+        return $this->_route;
+    }
+
+    public function getStatic()
+    {
+        return $this->_static;
     }
 }
