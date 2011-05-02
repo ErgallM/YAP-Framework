e89f6bf4ae13338930095e7470b9bfecdd4fcac7
Victor <stalin1945@gmail.com>
2011-05-02 21:45:21 +0300

    Fixed: Incorrect directories blacklist; Added: new tests for Route class

--- tests/Yap/Router/RouteTest.php
+++ tests/Yap/Router/RouteTest.php
@@ -1,10 +1,41 @@
 <?php
 class RouteTest extends PHPUnit_Framework_TestCase
 {
+    protected $_name = 'test1';
+    
+    protected $_routeRegexp = 'page/:pageNumber';
+    
+    protected $_route;
+    
+    public function setUp() 
+    {
+        parent::setUp();
+        $this->_route = new \Yap\Router\Route($this->_name, $this->_routeRegexp);
+    }
+    
+    public function testType() 
+    {
+        $this->assertType('\Yap\Router\Route', $this->_route);
+    }
+    
+    public function testCanGetName() 
+    {
+        $this->assertEquals($this->_name, $this->_route->getName());
+    }
+    
     public function testMatch()
     {
-        $route = new \Yap\Router\Route('test1', 'page/:pageNumber');
-        $this->assertFalse($route->match('pages'));
-        $this->assertFalse($route->match('pages/24/1'));
+        $this->assertInternalType('array', $this->_route->match('page'));
+        $this->assertInternalType('array', $this->_route->match('page/24'));
+        
+        //Test trimming ability
+        $this->assertInternalType('array', $this->_route->match('page/'));
+        $this->assertInternalType('array', $this->_route->match('page/24/'));
+    }
+    
+    public function testMisMatch()
+    {
+        $this->assertFalse($this->_route->match('pages'));
+        $this->assertFalse($this->_route->match('pages/24/1'));
     }
 }
